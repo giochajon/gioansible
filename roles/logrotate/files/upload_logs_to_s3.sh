5a9f770338ec01f37336e7b81324263a8bce9dc8
Giovanni Chajon <giovanni.chajon@curvedental.com>
2020-06-05 14:14:19 -0600

    added the logtotate role

---
+++ roles/logrotate/files/upload_logs_to_s3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Move the logfiles created by custom prerotate process to S3 bucket
+
+LOG_PATH=/tmp
+
+for file in ${LOG_PATH}/*.tar; do 
+  mypath=$(echo ${file} | awk -F '[-]' '{for(i=NF-2;i<=NF;i++) printf $i"/"; }' |  awk -F '[_]' '{print $1}');
+  echo aws s3 mv /tmp/"${file}" s3://curve-log-archive/"${mypath}"/herodb/"${file}" --dryrun >> /logs/;
+done
